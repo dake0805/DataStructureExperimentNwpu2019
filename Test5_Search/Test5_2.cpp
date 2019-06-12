d94117c26342fd265f95a1c7bddfafdc6fe19708
ZhiYang <34023806+dake0805@users.noreply.github.com>
2019-06-12 15:01:20 +0800

    612.2

--- Test5_Search/Test5_2.cpp
+++ Test5_Search/Test5_2.cpp
@@ -5,3 +5,45 @@
 //第二部分 第一题
 
 #include "Test5_2.h"
+
+void Test5_2::RunTest2()
+{
+
+    TreeNode *tree;
+    int initVal;
+    cout << "insert nodes, input -1 to end" << endl;
+    while (true)
+    {
+        cin >> initVal;
+        if (initVal == -1)
+            break;
+        insertNode(tree, initVal);
+    }
+    minimumDiff(tree);
+}
+
+int Test5_2::minimumDiff(TreeNode *root)
+{
+
+
+}
+
+void Test5_2::insertNode(TreeNode *root, int val)
+{
+    if (root == nullptr)
+        root = new TreeNode(val);
+    else if (root->val == x)
+        return;
+    else if (root->val > x)
+    {
+        insertNode(root->left, val);
+    } else if (root->val < x)
+    {
+        insertNode(root->right, val);
+    }
+}
+
+void Test5_2::dfs(TreeNode *root)
+{
+
+}
--- Test5_Search/Test5_2.h
+++ Test5_Search/Test5_2.h
@@ -5,9 +5,34 @@
 #ifndef DATASTRUCTUREEXPERIMENTNWPU2019_TEST5_2_H
 #define DATASTRUCTUREEXPERIMENTNWPU2019_TEST5_2_H
 
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {}
+};
 
 class Test5_2
 {
+public:
+    void RunTest2();
+
+    int minimumDiff(TreeNode *root);
+
+    void insertNode(TreeNode *root, int val);
+
+    void dfs(TreeNode *root);
+
+private:
+    TreeNode *pre = nullptr;
+    int res = INT32_MAX;
 
 };
 
